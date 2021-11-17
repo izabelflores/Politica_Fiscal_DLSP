4ddbf8e00395bdeec071ea597f782a180fd63f46
Izabel Flores <izabelflores9@gmail.com>
2021-11-17 19:46:23 -0300

    dividir listas por datas de inicio das séries

--- Limpeza_e_datas.R
+++ Limpeza_e_datas.R
@@ -6,8 +6,9 @@
 #%% Limpeza
 # (tirar NA's)
 
+#%% inicio da serie em 2001/12/01
 
-list <- list(DLSP, 
+list_1 <- list(DLSP, 
              fc_DLSP,
              fc_DLSP_det_cxcomp,
              fc_DLSP_det_estoques,
@@ -19,32 +20,60 @@ list <- list(DLSP,
              fc_DLSP_det_privat,
              fc_DLSP_det_recdiv,
              DBGG_2007,
-             DBGG_2008,
-             fc_DBGG,
-             fc_DBGG_det_ajcamext,
-             fc_DBGG_det_ajcamint,
-             fc_DBGG_det_divexoutros,
-             fc_DBGG_det_emissoes,
-             fc_DBGG_det_estoques,
-             fc_DBGG_det_juros,
-             fc_DBGG_det_privat,
-             fc_DBGG_det_recdiv
-             )
+             NFSP_fontes,
+             NFSP_usos,
+             tx_impl_DLSP,
+             cronop_DLSP)
 
-data <- lapply(list, function(x) {x <- x %>% filter(!is.na(x[,3])); return(x)})
+st <- as.Date("2001/12/01")
+seq <- seq(st, by = "month", along.with = (DLSP[1,]-2))
+
+data_1 <- lapply(list_1, function(x) {x <- x %>% 
+  filter(!is.na(x[,3])) %>% 
+  select(-c(1,2)) %>% 
+  `colnames<-`(seq); return(x)})
 
 
-#%% colocar data nas colunas 
-# (fazer tabela com com data de inicio de cada coluna)
-# função pra vetor com tamanho do data frame
-# vetor como nome das colunas
+#%% inicio da serie em 2006/12/01
+
+list_2 <- list(fc_DBGG,
+                  fc_DBGG_det_estoques,
+                  cronop_DBGG)
+
+st <- as.Date("2006/12/01")
+seq <- seq(st, by = "month", along.with = (fc_DBGG[1,]-2))
+
+data_2 <- lapply(list_2, function(x) {x <- x %>% 
+  filter(!is.na(x[,3])) %>% 
+  select(-c(1,2)) %>% 
+  `colnames<-`(seq); return(x)})
+
+
+#%% inicio da serie em 2007/01/01
+
+list_3 <- list(DBGG_2008,
+               fc_DBGG_det_ajcamext,
+               fc_DBGG_det_ajcamint,
+               fc_DBGG_det_divexoutros,
+               fc_DBGG_det_emissoes,
+               fc_DBGG_det_juros,
+               fc_DBGG_det_privat,
+               fc_DBGG_det_recdiv,
+               tx_impl_DBGG)
+
+st <- as.Date("2007/01/01")
+seq <- seq(st, by = "month", along.with = (DBGG_2008[1,]-2))
+
+data_3 <- lapply(list_3, function(x) {x <- x %>% 
+  filter(!is.na(x[,3])) %>% 
+  select(-c(1,2)) %>% 
+  `colnames<-`(seq); return(x)})
 
 # 18/11
 
-#%% o mesmo para outras
-#%% juntar todas as bases em uma (ou duas)
+#%% conferir se pegou todas as tabelas que estão na vertical
+#%% pegou as tabelas horizontais
 
 # 19/11
-
-#%% criar codigo e dicionario das variaveis
-#%% exportar para o excel
+#%% elaborar vetor de nomes das colunas
+#%% juntar todas as bases em uma (ou duas)
