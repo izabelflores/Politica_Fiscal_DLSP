526230e786b4904a7a5aaf9c3396d3f2725d7373
Izabel Flores <izabelflores9@gmail.com>
2021-11-16 23:05:12 -0300

    cronograma de limpeza

---
+++ Limpeza_e_datas.R
@@ -0,0 +1,21 @@
+
+# 17/11
+
+#DLSP e DBGG
+
+#%% Limpeza
+# (tirar NA's)
+#%% colocar data nas colunas 
+# (fazer tabela com com data de inicio de cada coluna)
+# função pra vetor com tamanho do data frame
+# vetor como nome das colunas
+
+# 18/11
+
+#%% o mesmo para outras
+#%% juntar todas as bases em uma (ou duas)
+
+# 19/11
+
+#%% criar codigo e dicionario das variaveis
+#%% exportar para o excel
