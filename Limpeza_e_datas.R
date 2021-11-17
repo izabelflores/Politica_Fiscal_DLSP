5abe035e07d54ba027bb10f28408e92e88516c69
Izabel Flores <izabelflores9@gmail.com>
2021-11-17 15:07:18 -0300

    lapply removendo NA's

--- Limpeza_e_datas.R
+++ Limpeza_e_datas.R
@@ -5,6 +5,35 @@
 
 #%% Limpeza
 # (tirar NA's)
+
+
+list <- list(DLSP, 
+             fc_DLSP,
+             fc_DLSP_det_cxcomp,
+             fc_DLSP_det_estoques,
+             fc_DLSP_det_juros,
+             fc_DLSP_det_metext,
+             fc_DLSP_det_metint,
+             fc_DLSP_det_paridade,
+             fc_DLSP_det_primario,
+             fc_DLSP_det_privat,
+             fc_DLSP_det_recdiv,
+             DBGG_2007,
+             DBGG_2008,
+             fc_DBGG,
+             fc_DBGG_det_ajcamext,
+             fc_DBGG_det_ajcamint,
+             fc_DBGG_det_divexoutros,
+             fc_DBGG_det_emissoes,
+             fc_DBGG_det_estoques,
+             fc_DBGG_det_juros,
+             fc_DBGG_det_privat,
+             fc_DBGG_det_recdiv
+             )
+
+data <- lapply(list, function(x) {x <- x %>% filter(!is.na(x[,3])); return(x)})
+
+
 #%% colocar data nas colunas 
 # (fazer tabela com com data de inicio de cada coluna)
 # função pra vetor com tamanho do data frame
